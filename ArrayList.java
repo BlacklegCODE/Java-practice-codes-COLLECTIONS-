d5d48f22c6270714f0488336bdc947f1d40abf7d
Harsh Raundal <dkingston0000@gmail.com>
2026-01-11 21:16:40 +0530

    Add example of ArrayList usage in Java
    Implemented a simple Java program that demonstrates the use of ArrayList by adding elements and printing the list.

---
+++ ArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList A1 = new ArrayList();
+
+        //add elements to array list
+
+        A1.add("Red");
+        A1.add("5");
+        A1.add("Null");
+
+
+        //printing the ArrayList
+
+        System.out.println(A1);
+    }
+}
