e6704fd00978d52ea8be65b91af8ae25cbc00f45
Harsh Raundal <dkingston0000@gmail.com>
2026-01-19 16:46:43 +0530

    Create ArrayList2.java

---
+++ ArrayList2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList <String> names = new ArrayList<>();
+
+        names.add("Sanji");
+        names.add("Zoro");
+        names.add("Luffy");
+
+        System.out.println(names);
+    }
+}
